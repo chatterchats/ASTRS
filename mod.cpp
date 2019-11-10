6b5c22323241502da4f0f7604925a7d9a9919f40
RTO-Chatter <57570001+RTO-Chatter@users.noreply.github.com>
2019-11-09 22:56:22 -0800

    Updating Icons

--- mod.cpp
+++ mod.cpp
@@ -1,12 +1,13 @@
 name = "TFAR LR Settings";
 tooltipOwned="TFAR LR Settings";
-picture = "";
-logoSmall = "";
-logo = "";
-logoOver = "";
+picture = "CHTR_TFAR_QoL\ui\chtr_tfar_qol_iconLG.paa";
+logoSmall = "CHTR_TFAR_QoL\ui\chtr_tfar_qol_iconLG.paa";
+logo = "CHTR_TFAR_QoL\ui\chtr_tfar_qol_iconLG.paa";
+logoOver = "CHTR_TFAR_QoL\ui\chtr_tfar_qol_iconLG.paa";
 hideName = 0;
 hidePicture = 0;
 actionName = "Github";
+author = "Chatter and M3ales";
 action = "https://github.com/RTO-Chatter/CHTR_TFAR_QoL/";
 description = "A Simple Quality of Life mod for the ARMA 3 TFAR Addon. Allows you to save your current configuration, and reload it at any point in the future until a new config is saved.s";
 overview = "A Simple Quality of Life mod for the ARMA 3 TFAR Addon. Allows you to save your current configuration, and reload it at any point in the future until a new config is saved.";
