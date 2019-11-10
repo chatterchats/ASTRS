b6373d67cd4386f5fdd87177bfe1f2a22a869aa1
RTO-Chatter <57570001+RTO-Chatter@users.noreply.github.com>
2019-11-09 23:15:22 -0800

    Update to mod.cpp

--- CHTR_TFAR_QOL/mod.cpp
+++ CHTR_TFAR_QOL/mod.cpp
@@ -1,4 +1,5 @@
 name = "ACE TFAR Long Range Setter";
+author = "Chatter and M3ales";
 tooltipOwned="ACE TFAR Long Range Setter";
 picture = "CHTR_TFAR_QoL\ui\lrsettings_iconLGNoTextAlpha.paa";
 logoSmall = "CHTR_TFAR_QoL\ui\lrsettings_iconLGNoTextAlpha.paa";
@@ -7,7 +8,6 @@ logoOver = "CHTR_TFAR_QoL\ui\lrsettings_iconLGNoTextAlpha.paa";
 hideName = 0;
 hidePicture = 0;
 actionName = "Github";
-author = "Chatter and M3ales";
 action = "https://github.com/RTO-Chatter/ACE-TFAR-Long-Range-Setter";
 description = "A Simple Quality of Life mod for the ARMA 3 TFAR Addon. Allows you to save your current configuration, and reload it at any point in the future until a new config is saved.s";
 overview = "A Simple Quality of Life mod for the ARMA 3 TFAR Addon. Allows you to save your current configuration, and reload it at any point in the future until a new config is saved.";
