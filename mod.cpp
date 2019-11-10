83c8f4e30b174e7b6ea441c8d229c4a7899498cc
RTO-Chatter <57570001+RTO-Chatter@users.noreply.github.com>
2019-11-09 22:59:33 -0800

    Removing vscode and updating mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,5 +1,5 @@
-name = "TFAR LR Settings";
-tooltipOwned="TFAR LR Settings";
+name = "ACE TFAR Long Range Setter";
+tooltipOwned="ACE TFAR Long Range Setter";
 picture = "CHTR_TFAR_QoL\ui\chtr_tfar_qol_iconLG.paa";
 logoSmall = "CHTR_TFAR_QoL\ui\chtr_tfar_qol_iconLG.paa";
 logo = "CHTR_TFAR_QoL\ui\chtr_tfar_qol_iconLG.paa";
@@ -8,6 +8,6 @@ hideName = 0;
 hidePicture = 0;
 actionName = "Github";
 author = "Chatter and M3ales";
-action = "https://github.com/RTO-Chatter/CHTR_TFAR_QoL/";
+action = "https://github.com/RTO-Chatter/ACE-TFAR-Long-Range-Setter";
 description = "A Simple Quality of Life mod for the ARMA 3 TFAR Addon. Allows you to save your current configuration, and reload it at any point in the future until a new config is saved.s";
 overview = "A Simple Quality of Life mod for the ARMA 3 TFAR Addon. Allows you to save your current configuration, and reload it at any point in the future until a new config is saved.";
