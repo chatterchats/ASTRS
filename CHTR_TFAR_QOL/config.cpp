e62463e5d266c9272b85df214471471386b61794
Mira <mattfdavies12@gmail.com>
2019-11-11 07:33:36 +0200

    formatted config, added requiredAddons for functions being built

--- CHTR_TFAR_QOL/config.cpp
+++ CHTR_TFAR_QOL/config.cpp
@@ -1,7 +1,5 @@
-class CfgPatches
-{
-	class CHTR_TFAR_QoL
-	{
+class CfgPatches {
+	class CHTR_TFAR_QoL {
 		name = "TFAR LR Settings";
 		author = "Chatter and M3ales";
 		url = "https://github.com/RTO-Chatter/CHTR_TFAR_QoL";
@@ -11,48 +9,43 @@ class CfgPatches
 	};
 };
 
-class CfgFunctions
-{
-	class CHTR_TFAR_QoL{
-        class TFARLRSettings{
+class CfgFunctions {
+	class CHTR_TFAR_QoL {
+        class TFARLRSettings {
             tag = "CHTR_TFAR_QoL";
-            requiredAddons[]={};
-            file="CHTR_TFAR_QoL\functions";
-			class loadLRSettings{};
-			class saveLRSettings{};
+            requiredAddons[] = {"ace", "task_force_radio"};
+            file = "CHTR_TFAR_QoL\functions";
+			class loadLRSettings {};
+			class saveLRSettings {};
         };
 	};
 };
 
-class CfgVehicles
-{
+class CfgVehicles {
     class Man;
     class CAManBase: Man {
         class ACE_SelfActions {
-			class ACE_Equipment{
+			class ACE_Equipment {
 				class CHTR_TFAR_QoL {
-					displayName="LR Settings";
-					showDisabled=0;
-					exceptions[]=
-						{
-							"isNotInside",
-							"isNotSwimming",
-							"isNotSitting"
-						};
-					condition = "call TFAR_fnc_haveLRRadio";
-					runOnHover=0;
-					icon="CHTR_TFAR_QoL\ui\interact_root.paa"
-					class CHTR_saveLRSettings
-					{
-						displayName="Save";
-						icon="CHTR_TFAR_QoL\ui\save.paa"
-						statement="call CHTR_TFAR_QoL_fnc_saveLRSettings";
+					displayName = "LR Settings";
+					showDisabled = 0;
+					exceptions[] = {
+						"isNotInside",
+						"isNotSwimming",
+						"isNotSitting"
 					};
-					class CHTR_loadLRSettings
-					{
-						displayName="Load";
-						icon="CHTR_TFAR_QoL\ui\load.paa"
-						statement="call CHTR_TFAR_QoL_fnc_loadLRSettings";
+					condition = "call TFAR_fnc_haveLRRadio";
+					runOnHover = 0;
+					icon="CHTR_TFAR_QoL\ui\interact_root.paa"
+					class CHTR_saveLRSettings {
+						displayName = "Save";
+						icon = "CHTR_TFAR_QoL\ui\save.paa"
+						statement = "call CHTR_TFAR_QoL_fnc_saveLRSettings";
+					};
+					class CHTR_loadLRSettings {
+						displayName = "Load";
+						icon = "CHTR_TFAR_QoL\ui\load.paa"
+						statement = "call CHTR_TFAR_QoL_fnc_loadLRSettings";
 					};
 				};
 			};
