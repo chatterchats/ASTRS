00e7e5194f9f1d54ddcec392f5f0b714031e132f
R. Chatter <octavewolfz@gmail.com>
2019-11-27 19:30:33 -0800

    * Fixed Tree & Conditions for ACE Interacts

--- CHTR_TFAR_Setter/CfgVehicles.hpp
+++ CHTR_TFAR_Setter/CfgVehicles.hpp
@@ -9,97 +9,99 @@ class CfgVehicles {
     class Man;
     class CAManBase: Man {
         class ACE_SelfActions {
-			class ADDON {
-				displayName = "TFAR Setter";
-				icon = QUOTE(ICON_PATH(interact_root));
-				exceptions[] = {
-					"isNotInside",
-					"isNotSwimming",
-					"isNotSitting",
-					"notOnMap"
-				};
-				class Load_Both {
-					displayName = "Load Both";
+			class ACE_Equipment {
+				class ADDON {
+					displayName = "TFAR Setter";
 					icon = QUOTE(ICON_PATH(interact_root));
-					statement = QUOTE([true] call FUNC(loadBothSettings));
-					condition = QUOTE(!ALTERNATE_LAYOUT && SHOW_LR && SHOW_SR);
-				};
-				//Original Layout, ALTERNATE_LAYOUT = False
-				class LR_Root {
-					displayName = "LR";
-					icon = QUOTE(ICON_PATH(lr));
-					statement = QUOTE(if(SHORTCUT_ENABLED) then {[true] call FUNC(loadLRSettings)};);
-					condition = QUOTE(!ALTERNATE_LAYOUT && SHOW_LR);
-					class LR_Load {
-						icon = QUOTE(ICON_PATH(load));
-						statement = QUOTE([true] call FUNC(loadLRSettings));
-						displayName = "Load";
-					};
-					class LR_Save {
-						icon = QUOTE(ICON_PATH(save));
-						statement = QUOTE([true] call FUNC(saveLRSettings));
-						displayName = "Save";
-					};
-				};
-				//Original Layout, ALTERNATE_LAYOUT = False
-				class SR_Root {
-					displayName = "SR";
-					icon = QUOTE(ICON_PATH(sr));
-					statement = QUOTE(if(SHORTCUT_ENABLED) then {[true] call FUNC(loadSRSettings)};);
-					condition = QUOTE(!ALTERNATE_LAYOUT && SHOW_SR && HAS_SR);
-					class SR_Load {
-						icon = QUOTE(ICON_PATH(load));
-						statement = QUOTE([true] call FUNC(loadSRSettings));
-						displayName = "Load";
-					};
-					class SR_Save {
-						icon = QUOTE(ICON_PATH(save));
-						statement = QUOTE([true] call FUNC(saveSRSettings));
-						displayName = "Save";
-					};
-				};
-				//New Layout, ALTERNATE_LAYOUT = True
-				class Save_Root {
-					displayName = "Save";
-					statement = "";
-					condition = QUOTE(ALTERNATE_LAYOUT && (SHOW_LR || SHOW_SR) && (HAS_LR || HAS_SR));
-					icon = QUOTE(ICON_PATH(save));
-					class Save_LR {
-						displayName = "LR";
-						icon = QUOTE(ICON_PATH(lr));
-						statement = QUOTE([true] call FUNC(saveLRSettings));
-						condition = QUOTE(ALTERNATE_LAYOUT && SHOW_LR);
-					};
-					class Save_SR {
-						icon = QUOTE(ICON_PATH(sr));
-						displayName = "SR";
-						statement = QUOTE([true] call FUNC(saveSRSettings));
-						condition = QUOTE(ALTERNATE_LAYOUT && SHOW_SR);
-					};
-				};
-				//New Layout, ALTERNATE_LAYOUT = True
-				class Load_Root {
-					displayName = "Load";
-					icon = QUOTE(ICON_PATH(load));
-					statement = QUOTE(if(SHORTCUT_ENABLED && SHOW_LR && SHOW_SR) then {[true] call FUNC(loadBothSettings)};);
-					condition = QUOTE(ALTERNATE_LAYOUT);
-					class Load_LR {
-						displayName = "LR";
-						icon = QUOTE(ICON_PATH(lr));
-						statement = QUOTE([true] call FUNC(loadLRSettings));
-						condition = QUOTE(ALTERNATE_LAYOUT && (SHOW_LR));
-					};
-					class Load_SR {
-						displayName = "SR";
-						icon = QUOTE(ICON_PATH(sr));
-						statement = QUOTE([true] call FUNC(loadSRSettings));
-						condition = QUOTE(ALTERNATE_LAYOUT && SHOW_SR);
+					exceptions[] = {
+						"isNotInside",
+						"isNotSwimming",
+						"isNotSitting",
+						"notOnMap"
 					};
 					class Load_Both {
-						displayName = "Both";
+						displayName = "Load Both";
 						icon = QUOTE(ICON_PATH(interact_root));
 						statement = QUOTE([true] call FUNC(loadBothSettings));
-						condition = QUOTE(ALTERNATE_LAYOUT && SHOW_SR && SHOW_LR);
+						condition = QUOTE(!ALTERNATE_LAYOUT && SHOW_LR && SHOW_SR && HAS_LR && HAS_SR);
+					};
+					//Original Layout, ALTERNATE_LAYOUT = False
+					class LR_Root {
+						displayName = "LR";
+						icon = QUOTE(ICON_PATH(lr));
+						statement = QUOTE(if(SHORTCUT_ENABLED) then {[true] call FUNC(loadLRSettings)};);
+						condition = QUOTE(!ALTERNATE_LAYOUT && SHOW_LR && HAS_LR);
+						class LR_Load {
+							icon = QUOTE(ICON_PATH(load));
+							statement = QUOTE([true] call FUNC(loadLRSettings));
+							displayName = "Load";
+						};
+						class LR_Save {
+							icon = QUOTE(ICON_PATH(save));
+							statement = QUOTE([true] call FUNC(saveLRSettings));
+							displayName = "Save";
+						};
+					};
+					//Original Layout, ALTERNATE_LAYOUT = False
+					class SR_Root {
+						displayName = "SR";
+						icon = QUOTE(ICON_PATH(sr));
+						statement = QUOTE(if(SHORTCUT_ENABLED) then {[true] call FUNC(loadSRSettings)};);
+						condition = QUOTE(!ALTERNATE_LAYOUT && SHOW_SR && HAS_SR);
+						class SR_Load {
+							icon = QUOTE(ICON_PATH(load));
+							statement = QUOTE([true] call FUNC(loadSRSettings));
+							displayName = "Load";
+						};
+						class SR_Save {
+							icon = QUOTE(ICON_PATH(save));
+							statement = QUOTE([true] call FUNC(saveSRSettings));
+							displayName = "Save";
+						};
+					};
+					//New Layout, ALTERNATE_LAYOUT = True
+					class Save_Root {
+						displayName = "Save";
+						statement = "";
+						condition = QUOTE(ALTERNATE_LAYOUT && (SHOW_LR || SHOW_SR) && (HAS_LR || HAS_SR));
+						icon = QUOTE(ICON_PATH(save));
+						class Save_LR {
+							displayName = "LR";
+							icon = QUOTE(ICON_PATH(lr));
+							statement = QUOTE([true] call FUNC(saveLRSettings));
+							condition = QUOTE(ALTERNATE_LAYOUT && SHOW_LR && HAS_LR);
+						};
+						class Save_SR {
+							icon = QUOTE(ICON_PATH(sr));
+							displayName = "SR";
+							statement = QUOTE([true] call FUNC(saveSRSettings));
+							condition = QUOTE(ALTERNATE_LAYOUT && SHOW_SR && HAS_SR);
+						};
+					};
+					//New Layout, ALTERNATE_LAYOUT = True
+					class Load_Root {
+						displayName = "Load";
+						icon = QUOTE(ICON_PATH(load));
+						statement = QUOTE(if(SHORTCUT_ENABLED && SHOW_LR && SHOW_SR) then {[true] call FUNC(loadBothSettings)};);
+						condition = QUOTE(ALTERNATE_LAYOUT);
+						class Load_LR {
+							displayName = "LR";
+							icon = QUOTE(ICON_PATH(lr));
+							statement = QUOTE([true] call FUNC(loadLRSettings));
+							condition = QUOTE(ALTERNATE_LAYOUT && (SHOW_LR) && HAS_LR);
+						};
+						class Load_SR {
+							displayName = "SR";
+							icon = QUOTE(ICON_PATH(sr));
+							statement = QUOTE([true] call FUNC(loadSRSettings));
+							condition = QUOTE(ALTERNATE_LAYOUT && SHOW_SR && HAS_sR);
+						};
+						class Load_Both {
+							displayName = "Both";
+							icon = QUOTE(ICON_PATH(interact_root));
+							statement = QUOTE([true] call FUNC(loadBothSettings));
+							condition = QUOTE(ALTERNATE_LAYOUT && SHOW_SR && SHOW_LR && HAS_LR && HAS_SR);
+						};
 					};
 				};
 			};
@@ -108,24 +110,26 @@ class CfgVehicles {
     class Air;
     class Helicopter: Air {
         class ACE_SelfActions {
-			class ADDON {	
-				displayName = "TFAR Setter";
-				icon = QUOTE(ICON_PATH(interact_root));
-				//Original Layout, ALTERNATE_LAYOUT = False
-				class LR_Root {
-					displayName = "Vehicle LR";
-					icon = QUOTE(ICON_PATH(lr));
-					statement = QUOTE(if(SHORTCUT_ENABLED) then {[true] call FUNC(loadLRSettings)};);
-					condition = QUOTE(SHOW_LR);
-					class LR_Load {
-						icon = QUOTE(ICON_PATH(load));
-						statement = QUOTE([true] call FUNC(loadVLRSettings));
-						displayName = "Load";
-					};
-					class LR_Save {
-						icon = QUOTE(ICON_PATH(save));
-						statement = QUOTE([true] call FUNC(saveVLRSettings));
-						displayName = "Save";
+			ACE_Equipment{
+				class ADDON {	
+					displayName = "TFAR Setter";
+					icon = QUOTE(ICON_PATH(interact_root));
+					//Original Layout, ALTERNATE_LAYOUT = False
+					class LR_Root {
+						displayName = "Vehicle LR";
+						icon = QUOTE(ICON_PATH(lr));
+						statement = QUOTE(if(SHORTCUT_ENABLED) then {[true] call FUNC(loadLRSettings)};);
+						condition = QUOTE(SHOW_LR && && HAS_LR);
+						class LR_Load {
+							icon = QUOTE(ICON_PATH(load));
+							statement = QUOTE([true] call FUNC(loadVLRSettings));
+							displayName = "Load";
+						};
+						class LR_Save {
+							icon = QUOTE(ICON_PATH(save));
+							statement = QUOTE([true] call FUNC(saveVLRSettings));
+							displayName = "Save";
+						};
 					};
 				};
 			};
