068d23ef34aa6054fc259adfc4d929252c750abd
m3ales <mattfdavies12@gmail.com>
2019-12-27 11:03:13 +0200

    Updated default settings array to match new data

--- CHTR_TFAR_Setter/functions/defaults.hpp
+++ CHTR_TFAR_Setter/functions/defaults.hpp
@@ -1,5 +1,5 @@
 //[currentProfileIndex, [profileID, profileDisplayName, lrData, srData, prefs]]
-#define SETTINGS_DEFAULT [0, [0, "Default Profile", [], [], []]]
+#define SETTINGS_DEFAULT [0, [0, "Default Profile", [[]], [], []]]
 //[showLR, showSR, alternateLayout, loadShortcutsEnabled]
 #define PREFS_DEFAULT [true, true, false, false]
 #define PREFS_INDEX 4
