67aa398e9a0741b74c92c9214b90287fe28235c7
Chatter <octavewolfz@gmail.com>
2019-12-27 22:56:06 -0800

    Because better logging?

--- CHTR_TFAR_Setter/functions/function_macros.hpp
+++ CHTR_TFAR_Setter/functions/function_macros.hpp
@@ -2,3 +2,4 @@
 #define LOG_BASE(level,msg) diag_log text format[QUOTE(ADDON[level]: %1), msg]
 #define LOG(msg) LOG_BASE(DEBUG,msg)
 #define LOG_ERROR(msg) LOG_BASE(ERR,msg)
+#define LOG_FILENUMBER(level,msg) format [ARR_4('%1 %2:%3',msg,__FILE__,__LINE__ + 1)]
