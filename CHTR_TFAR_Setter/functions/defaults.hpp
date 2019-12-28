eca7aa8510ebc7868a3cb6ea6afc03b6ac7e8576
Chatter <octavewolfz@gmail.com>
2019-12-28 13:26:48 -0800

    Updated to match new default format

--- CHTR_TFAR_Setter/functions/defaults.hpp
+++ CHTR_TFAR_Setter/functions/defaults.hpp
@@ -1,5 +1,5 @@
 //[currentProfileIndex, [profileID, profileDisplayName, lrData, srData, prefs]]
-#define SETTINGS_DEFAULT [0, [0, "Default Profile", [[]], [], []]]
+#define SETTINGS_DEFAULT [0, [0, "Default Profile", [[], []], [], []]]
 //Data Indexes(Highest Level of array)
 #define CURRENTPROFILE_ID 0
 //Profile Indexes (First nested array)
