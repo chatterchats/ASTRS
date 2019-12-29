ffb52069cd4d8ef633cad68c8fe0b69726128296
m3ales <mattfdavies12@gmail.com>
2019-12-29 17:05:34 +0200

    Filenumbers for log entries are in, defined arr_3 and 5 (#9)

--- CHTR_TFAR_Setter/config_macros.hpp
+++ CHTR_TFAR_Setter/config_macros.hpp
@@ -7,4 +7,6 @@
 #define COMPILE_FILE(name) compile preprocessFileLineNumbers 'ADDON\##name##.sqf'
 #define GVAR(name) ADDON##_##name
 #define ARR_2(a,b) a, b
+#define ARR_3(a,b,c) a, b, c
 #define ARR_4(a,b,c,d) a, b, c, d
+#define ARR_5(a,b,c,d,e) a, b, c, d, e
--- CHTR_TFAR_Setter/functions/function_macros.hpp
+++ CHTR_TFAR_Setter/functions/function_macros.hpp
@@ -1,5 +1,10 @@
 #include "../config_macros.hpp"
 #define LOG_BASE(level,msg) diag_log text format[QUOTE(ADDON[level]: %1), msg]
-#define LOG(msg) LOG_BASE(DEBUG,msg)
-#define LOG_ERROR(msg) LOG_BASE(ERR,msg)
-#define LOG_FILENUMBER(level,msg) format [ARR_4('%1 %2:%3',msg,__FILE__,__LINE__ + 1)]
+#define LOG(msg) LOG_BASE(DEBUG, LOG_FILENUMBER(msg))
+#define LOGF_1(msg,arg0) LOG_BASE(DEBUG, format[ARR_2(msg,arg0)])
+#define LOGF_2(msg,arg0,arg1) LOG_BASE(DEBUG, format[ARR_3(msg,arg0,arg1)])
+#define LOGF_3(msg,arg0,arg1,arg2) LOG_BASE(DEBUG, format[ARR_4(msg,arg0,arg1,arg2)])
+#define LOG_ERROR(msg) LOG_BASE(ERR,LOG_FILENUMBER(msg))
+#define LOG_ERRORF_1(msg,arg0) LOG_ERROR(format[ARR_2(msg,arg0)])
+#define LOG_ERRORF_2(msg,arg0,arg1) LOG_ERROR(format[ARR_3(msg,arg0,arg1)])
+#define LOG_FILENUMBER(msg) format [ARR_4('%1 at %2:%3',msg,__FILE__,__LINE__ + 1)]
