382b57b68da3d9c0fec470b399b075e611935879
m3ales <mattfdavies12@gmail.com>
2019-12-29 17:22:35 +0200

    Made LOGF call LOG as a base to keep consistent (#9)

--- CHTR_TFAR_Setter/functions/function_macros.hpp
+++ CHTR_TFAR_Setter/functions/function_macros.hpp
@@ -1,9 +1,9 @@
 #include "../config_macros.hpp"
 #define LOG_BASE(level,msg) diag_log text format[QUOTE(ADDON[level]: %1), msg]
 #define LOG(msg) LOG_BASE(DEBUG, LOG_FILENUMBER(msg))
-#define LOGF_1(msg,arg0) LOG_BASE(DEBUG, format[ARR_2(msg,arg0)])
-#define LOGF_2(msg,arg0,arg1) LOG_BASE(DEBUG, format[ARR_3(msg,arg0,arg1)])
-#define LOGF_3(msg,arg0,arg1,arg2) LOG_BASE(DEBUG, format[ARR_4(msg,arg0,arg1,arg2)])
+#define LOGF_1(msg,arg0) LOG(format[ARR_2(msg,arg0)])
+#define LOGF_2(msg,arg0,arg1) LOG(format[ARR_3(msg,arg0,arg1)])
+#define LOGF_3(msg,arg0,arg1,arg2) LOG(format[ARR_4(msg,arg0,arg1,arg2)])
 #define LOG_ERROR(msg) LOG_BASE(ERR,LOG_FILENUMBER(msg))
 #define LOG_ERRORF_1(msg,arg0) LOG_ERROR(format[ARR_2(msg,arg0)])
 #define LOG_ERRORF_2(msg,arg0,arg1) LOG_ERROR(format[ARR_3(msg,arg0,arg1)])
